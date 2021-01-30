6676a6f0277d6133711d62787ffa35d1f2071250
Victor Oda <victor.ra.oda@gmail.com>
2021-01-30 15:37:43 -0300

    Adicionado favicons

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,12 @@ export default function Home() {
         <meta property="og:title" content={db.title} />
         <meta property="og:description" content={db.description} />
         <meta property="og:image" content={db.bg} />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <QuizContainer>
         <QuizLogo />
